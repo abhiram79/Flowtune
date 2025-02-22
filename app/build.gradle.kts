4770754b09500b0a42085387fdd3f8619b59a009
abhiram79 <abhiramv670@gmail.com>
2025-02-22 22:30:45 +0530

    increment version code

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         applicationId = "com.abhiram.flowtune"
         minSdk = 21
         targetSdk = 35
-        versionCode = 2
-        versionName = "1.1"
+        versionCode = 3
+        versionName = "1.2"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
     buildTypes {
