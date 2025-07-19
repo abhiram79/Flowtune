a42ef37340c3b10c2288cf7a5719e0b85c0cad76
abhiram79 <abhiramv670@gmail.com>
2025-07-19 19:06:30 +0530

    package: Renamed to com.abhiram.flowtune

--- app/src/main/java/com/arturo254/opentune/models/ItemsPage.kt
+++
@@ -1,8 +0,0 @@
-package com.abhiram.flowtune.models
-
-import com.arturo254.innertube.models.YTItem
-
-data class ItemsPage(
-    val items: List<YTItem>,
-    val continuation: String?,
-)
--- app/src/main/java/com/arturo254/opentune/models/SimilarRecommendation.kt
+++
@@ -1,9 +0,0 @@
-package com.abhiram.flowtune.models
-
-import com.arturo254.innertube.models.YTItem
-import com.abhiram.flowtune.db.entities.LocalItem
-
-data class SimilarRecommendation(
-    val title: LocalItem,
-    val items: List<YTItem>,
-)
