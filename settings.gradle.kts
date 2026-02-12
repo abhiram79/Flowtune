8af66adf9e865ecf34e76a46c8240c20c00dcf37
abhiram79 <abhiramv670@gmail.com>
2026-02-12 12:02:32 +0530

    Restructured project layout by grouping all submodules to packages directory

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -16,28 +16,20 @@ dependencyResolutionManagement {
 //     id("org.gradle.toolchains.foojay-resolver-convention") version("1.0.0")
 // }
 
-rootProject.name = "Metrolist"
+rootProject.name = "Flowtune"
 include(":app")
 include(":innertube")
-include(":kugou")
-include(":lrclib")
-include(":kizzy")
-include(":lastfm")
-include(":betterlyrics")
-include(":simpmusic")
+include(":packages:kugou")
+include(":packages:lrclib")
+include(":packages:kizzy")
+include(":packages:lastfm")
+include(":packages:betterlyrics")
+include(":packages:simpmusic")
 
-// Use a local copy of NewPipe Extractor by uncommenting the lines below.
-// We assume, that Metrolist and NewPipe Extractor have the same parent directory.
-// If this is not the case, please change the path in includeBuild().
-//
-// For this to work you also need to change the implementation in innertube/build.gradle.kts
-// to one which does not specify a version.
-// From:
-//      implementation(libs.newpipe.extractor)
-// To:
-//      implementation("com.github.teamnewpipe:NewPipeExtractor")
-//includeBuild("../NewPipeExtractor") {
-//    dependencySubstitution {
-//        substitute(module("com.github.teamnewpipe:NewPipeExtractor")).using(project(":extractor"))
-//    }
-//}
+project(":packages:betterlyrics").projectDir = file("packages/betterlyrics")
+project(":packages:kizzy").projectDir = file("packages/kizzy")
+project(":packages:kugou").projectDir = file("packages/kugou")
+project(":packages:lastfm").projectDir = file("packages/lastfm")
+project(":packages:lrclib").projectDir = file("packages/lrclib")
+project(":packages:simpmusic").projectDir = file("packages/simpmusic")
+
