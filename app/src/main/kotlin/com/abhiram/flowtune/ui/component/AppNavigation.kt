d226ed2c387bc6a4020079c75bb3f0ee2d484e96
abhiram79 <abhiramv670@gmail.com>
2026-05-22 16:39:32 +0000

    added missing imports

--- app/src/main/kotlin/com/abhiram/flowtune/ui/component/AppNavigation.kt
+++ app/src/main/kotlin/com/abhiram/flowtune/ui/component/AppNavigation.kt
@@ -20,6 +20,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.Immutable
 import androidx.compose.runtime.Stable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
